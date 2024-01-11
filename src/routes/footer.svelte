a8a707bf946bfbd97763019dd0c55486a9c07064
3khoisthebest <aaqilnazeem.mohammedibrahim.404@k12.friscoisd.org>
2024-01-11 16:24:59 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/routes/footer.svelte
@@ -0,0 +1,6 @@
+<footer>
+	<a href="https://intoabyss.org/discord" target="_blank">Discord</a>
+	<a href="https://intoabyss.org/github" target="_blank">GitHub</a>
+	<a href="https://intoabyss.org/apply" target="_blank">Apply for Staff</a>
+	<p>Made by Paxton Warin</p>
+</footer>
